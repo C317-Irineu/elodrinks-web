b2d2d86ef634316b43c456945f2f22e5de47105f
Laura Pivoto <laurapivoto1@gmail.com>
2025-04-09 10:49:58 -0300

    feat/create 3 sections page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
-// app/page.tsx
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-[url('/background.png')] bg-cover bg-center">
-      <footer className="row-start-3 w-full px-8">
-        <h1 className="text-4xl sm:text-6xl font-bold text-white text-left font-alumni">
-          ELO DRINKS
+    <main className="w-full">
+      <section
+        className="h-screen bg-[url('/background.png')] bg-cover bg-center flex items-end px-8 py-16"
+      >
+        <h1 className="text-white text-6xl font-alumni text-left">
+          Seu texto aqui
         </h1>
-      </footer>
-    </div>
+      </section>
+
+      <section className="h-screen bg-yellow-100 flex items-center justify-center">
+        <h2 className="text-4xl font-bold text-gray-800 font-alumni">Seção 2</h2>
+      </section>
+
+      <section className="h-screen bg-yellow-100 flex items-center justify-center">
+        <h2 className="text-4xl font-bold text-gray-800 font-alumni">Seção 3</h2>
+      </section>
+    </main>
   );
 }

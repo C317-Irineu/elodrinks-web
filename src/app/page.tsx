ab3525194d125d352a5fb0da9be3e268d17fcec3
Laura Pivoto <laurapivoto1@gmail.com>
2025-04-09 19:26:22 -0300

    feat/add first sections buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
 export default function Home() {
   return (
     <main className="w-full">
-      <section
-        className="h-screen bg-[url('/background.png')] bg-cover bg-center flex items-end px-8 py-16"
-      >
-        <h1 className="text-white title font-bold tracking-wider text-left">
-        ELO DRINKS
-        </h1>
-        </section>
+      <section className="h-screen bg-[url('/background.png')] bg-cover bg-center relative px-8">
+        <div className="absolute top-4 right-4 flex space-x-4">
+          <button className="border-[2px] border-[#343230] bg-transparent text-[#343230] font-bold py-1 px-2 rounded-md">
+            Fazer um orçamento
+          </button>
+          <button className="border-[2px] border-[#343230] bg-transparent text-[#343230] font-bold py-1 px-2 rounded-md">
+              Quem somos
+          </button>
+          <button>
+          <img src="/instagram_icon.png" alt="Ícone" className="w-10 h-10" />
+          </button>
+        </div>
+
+        <div className="h-full flex items-end">
+          <h1 className="text-white title font-bold tracking-wider text-left">
+            ELO DRINKS
+          </h1>
+        </div>
+      </section>
 
       <section className="h-screen bg-[#FFF3E4] flex items-center justify-center">
         <h2 className="text-4xl font-bold text-gray-800 font-alumni">Seção 2</h2>

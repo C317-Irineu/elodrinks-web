142d84a45e8fdb0860c80f6e586456c039ed5426
Laura Pivoto <laurapivoto1@gmail.com>
2025-04-15 09:45:17 -0300

    fix/first section and third section first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,27 @@
 'use client';
 
+import FirstSection from './first-section';
+import Form from './client-info';
+
 export default function Home() {
   return (
     <main className="w-full">
-      <section className="h-screen bg-[url('/background.png')] bg-cover bg-center relative px-8">
-        <div className="absolute top-4 right-4 flex space-x-4">
-            <button
-              onClick={() => window.location.href = '/suaInfo'}
-              className="border-[2px] border-[#343230] bg-transparent text-[#343230] font-bold py-1 px-2 rounded-md"
-            >
-              Fazer um orçamento
-            </button>
-          <button className="border-[2px] border-[#343230] bg-transparent text-[#343230] font-bold py-1 px-2 rounded-md">
-              Quem somos
-          </button>
-          <button>
-            <img src="/instagram_icon.png" alt="Ícone" className="w-10 h-10" />
-          </button>
-        </div>
-
-        <div className="h-full flex items-end">
-          <h1 className="text-white title font-bold tracking-wider text-left">
-            ELO DRINKS
-          </h1>
-        </div>
+      <section className="w-full">
+        <FirstSection />
       </section>
 
-      <section className="h-screen bg-[#FFF3E4] flex items-center justify-center">
+      <section
+        id="second-section"
+        className="h-screen bg-[#FFF3E4] flex items-center justify-center"
+      >
         <h2 className="text-4xl font-bold text-gray-800 font-alumni">Seção 2</h2>
       </section>
 
-      <section className="h-screen bg-[#FFF3E4] flex items-center justify-center">
-        <h2 className="text-4xl font-bold text-gray-800 font-alumni">Seção 3</h2>
+      <section
+        id="third-section"
+        className="h-screen bg-[#FFF3E4] flex flex-col items-center justify-start pt-10"
+      >
+        <Form />
       </section>
     </main>
   );
